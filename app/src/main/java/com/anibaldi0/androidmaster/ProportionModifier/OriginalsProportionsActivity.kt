200793cf006ee3285d139783aa1fc26bea1d7867
AniDev <anibalcaeiro@gmail.com>
2023-09-16 19:48:06 -0300

    ingreso y borrado de medidas del molde

---
+++ app/src/main/java/com/anibaldi0/androidmaster/ProportionModifier/OriginalsProportionsActivity.kt
@@ -0,0 +1,12 @@
+package com.anibaldi0.androidmaster.ProportionModifier
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anibaldi0.androidmaster.R
+
+class OriginalsProportionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_originals_proportions)
+    }
+}
--- app/src/main/java/com/anibaldi0/androidmaster/ProportionModifier/ProportionModifierActivity.kt
+++ app/src/main/java/com/anibaldi0/androidmaster/ProportionModifier/ProportionModifierActivity.kt
@@ -3,6 +3,7 @@ package com.anibaldi0.androidmaster.ProportionModifier
 import android.hardware.camera2.params.MeteringRectangle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
@@ -14,16 +15,33 @@ class ProportionModifierActivity : AppCompatActivity() {
     private var isRectangleSelected: Boolean = true
     private var isRoundSelected: Boolean = false
 
+    private var originalHeight: Int = 1
+    private var originalWidth: Int = 1
+    private var originalLength: Int =  1
+    private var originalDiameter: Int = 1
+
+    private  var originalQuantityMolds: Int = 1
+
     private lateinit var cardViewRectangle: CardView
     private lateinit var cardViewRound: CardView
     private lateinit var rectangleImage: ImageView
     private lateinit var rectangleText: TextView
     private lateinit var roundImage: ImageView
     private lateinit var roundText: TextView
+    private lateinit var textViewCakePan: TextView
+    private lateinit var textViewMesureDiameter: TextView
+    private lateinit var editTextNumberDiameter: EditText
+    private lateinit var textViewMesureWidth: TextView
+    private lateinit var editTextNumberWidth: EditText
+    private lateinit var textViewMesureLength: TextView
+    private lateinit var editTextNumberLength: EditText
+
+    private lateinit var cardViewButtonNext: CardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_proportion_modifier)
+        
         initComponents()
         initListeners()
         initUI()
@@ -36,17 +54,54 @@ class ProportionModifierActivity : AppCompatActivity() {
         rectangleText = findViewById(R.id.rectangleText)
         roundImage = findViewById(R.id.roundImage)
         roundText = findViewById(R.id.roundText)
+        textViewCakePan = findViewById(R.id.textViewCakePan)
+        textViewMesureDiameter = findViewById(R.id.textViewMesureDiameter)
+        editTextNumberDiameter = findViewById(R.id.editTextNumberDiameter)
+        textViewMesureWidth = findViewById(R.id.textViewMesureWidth)
+        editTextNumberWidth = findViewById(R.id.editTextNumberWidth)
+        textViewMesureLength = findViewById(R.id.textViewMesureLength)
+        editTextNumberLength = findViewById(R.id.editTextNumberLength)
+
+        cardViewButtonNext = findViewById((R.id.cardViewButtonNext))
     }
 
     private fun initListeners() {
         cardViewRectangle.setOnClickListener {
             changeMoldForm()
             setMoldFormColor()
+            textViewCakePan.text = "Rectangular"
+            textViewMesureDiameter.setTextColor(ContextCompat.getColor(this, R.color.gray))
+            textViewMesureWidth.setTextColor(ContextCompat.getColor(this, R.color.black))
+            textViewMesureLength.setTextColor(ContextCompat.getColor(this, R.color.black))
+            editTextNumberDiameter.isEnabled = false
+            editTextNumberDiameter.text.clear()
+            editTextNumberWidth.isEnabled = true
+            editTextNumberLength.isEnabled = true
         }
         cardViewRound.setOnClickListener {
             changeMoldForm()
             setMoldFormColor()
+            textViewCakePan.text = "Round"
+            textViewMesureDiameter.setTextColor(ContextCompat.getColor(this, R.color.black))
+            textViewMesureWidth.setTextColor(ContextCompat.getColor(this, R.color.gray))
+            textViewMesureLength.setTextColor(ContextCompat.getColor(this, R.color.gray))
+            editTextNumberWidth.isEnabled = false
+            editTextNumberLength.isEnabled = false
+            editTextNumberWidth.text.clear()
+            editTextNumberLength.text.clear()
+            editTextNumberDiameter.isEnabled = true
+
+
         }
+        cardViewButtonNext.setOnClickListener{
+            volumeCalculate()
+        }
+    }
+
+
+
+    private fun volumeCalculate() {
+        TODO("Not yet implemented")
     }
 
     private fun changeMoldForm(){
@@ -83,9 +138,9 @@ class ProportionModifierActivity : AppCompatActivity() {
 
     private fun getBackgroundColor(isSelectedComponent:Boolean): Int {
         val colorReference = if(isSelectedComponent){
-            R.color.background_second_component_selected
+            R.color.teal_200
         }else{
-            R.color.background_second_component
+            R.color.teal_700
         }
         return  ContextCompat.getColor(this, colorReference)
     }
