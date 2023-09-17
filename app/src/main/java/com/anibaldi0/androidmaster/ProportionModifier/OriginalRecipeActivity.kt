95e5f5ec4a83c274e50f8c1368dd442c71a0736d
AniDev <anibalcaeiro@gmail.com>
2023-09-17 20:37:46 -0300

    agregado de edit text para ingresar ingredientes

--- app/src/main/java/com/anibaldi0/androidmaster/ProportionModifier/OriginalRecipeActivity.kt
+++ app/src/main/java/com/anibaldi0/androidmaster/ProportionModifier/OriginalRecipeActivity.kt
@@ -2,6 +2,8 @@ package com.anibaldi0.androidmaster.ProportionModifier
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.TextView
 import com.anibaldi0.androidmaster.R
 
@@ -13,6 +15,36 @@ class OriginalRecipeActivity : AppCompatActivity() {
         val textViewTotalCurrentVolume = findViewById<TextView>(R.id.textViewTotalCurrentVolume)
         val currentVolume: String = intent.extras?.getString("VOLUME_ROUND").orEmpty()
         textViewTotalCurrentVolume.text = "$currentVolume ml"
+
+        val autoCompleteTextView = arrayOf(
+            R.id.ingredient01,
+            R.id.ingredient02,
+            R.id.ingredient03,
+            R.id.ingredient04,
+            R.id.ingredient05,
+            R.id.ingredient06,
+            R.id.ingredient07,
+            R.id.ingredient08,
+            R.id.ingredient09
+        )
+        for (i in autoCompleteTextView.indices) {
+            val autoCompleteTextView = findViewById<AutoCompleteTextView>(autoCompleteTextView[i])
+            val suggestions = when (i) {
+                0 -> resources.getStringArray(R.array.suggestions)
+                1 -> resources.getStringArray(R.array.suggestions)
+                2 -> resources.getStringArray(R.array.suggestions)
+                3 -> resources.getStringArray(R.array.suggestions)
+                4 -> resources.getStringArray(R.array.suggestions)
+                5 -> resources.getStringArray(R.array.suggestions)
+                6 -> resources.getStringArray(R.array.suggestions)
+                7 -> resources.getStringArray(R.array.suggestions)
+                8 -> resources.getStringArray(R.array.suggestions)
+                else -> emptyArray() // Manejar cualquier otro caso
+            }
+            val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, suggestions)
+            autoCompleteTextView.setAdapter(adapter)
+        }
+
 
     }
 }
